373c10a0a8a37831001b37b1e13c950f9a4a0539
Arzach <ruslan--47@yandex.ru>
2016-06-04 02:56:37 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+
+const soy = require('./node_modules/clobl/soy').setOptions({
+    closureLibrary: path.join(
+        __dirname,
+        'node_modules/google-closure-library'
+    ),
+    closureTemplates: path.join(
+        __dirname,
+        'node_modules/closure-templates'
+    )
+});
+
+soy.loadFiles([path.join(__dirname, 'build/compiledServerSoy/server.soy.concat.js')]);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+	var html = soy.render('tr.lProfile.Template.base');
+
+    res.header('Content-Type', 'text/html; charset=utf-8');
+    res.end(html);	
+});
+
+app.listen(3000, function() {
+    console.log('Running at port %s', 3000);
+});
+
+/*, {params: {
+		profession: req.params.profession
+	}}*/
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var exec = require('child_process').exec;
+var path = require('path');
+
+const gulpHelper =
+    require('./node_modules/clobl/gulp-helper.js')
+        .use(gulp)
+        .setPath({
+            root: __dirname,
+            blocks: path.join(__dirname, '/app/blocks')
+        })
+        .setSoyPath({
+            root: 'build'
+        });
+
+var errorHandler = function (error) {
+  notify.onError({
+    message:  "Error: <%= error.message %>",
+  })(error);
+}
+
+
+gulp.task('sass', function () {
+  gulp.src('scss/*.scss')
+    .pipe(sass()).on('error', errorHandler)
+    .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('soy', function () {
+    return gulpHelper.soy.build();
+});
+
+gulp.task('scripts', function() {   
+  var compile = false;
+  var command = path.join(__dirname, 'node_modules/google-closure-library/closure/bin/build/closurebuilder.py');
+    command += ' --root="' + path.join(__dirname, 'app/blocks/') + '"';
+    command += ' --root="' + path.join(__dirname, 'node_modules/google-closure-library/') + '"';
+    command += ' --namespace="fn.lProfile.Profile"';    
+    if(compile) {
+      command += ' --output_mode=compiled';
+      command += ' --compiler_jar=compiler.jar';
+      command += ' --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS"';
+    } else {
+      command += ' --output_mode="script"';
+    }
+    command += ' --output_file="' + path.join(__dirname, ' build/js/profile.js') + '"';
+  exec(command);
+})
+
+gulp.task('watch', function () {
+  gulp.watch('scss/**', ['sass']);
+});
